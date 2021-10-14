da6c5d37fbc03c4d8070a1d872ea09d1fea074c1
Loumonth Jack <ljack@kisticket.com>
2021-10-14 00:00:55 -0500

    feat: access naming changes

--- controller/routes/private/dashboard.js
+++ controller/routes/private/dashboard.js
@@ -25,59 +25,56 @@ const {
 
 const updateAccess = (req, res, next) => {
     const userId = req.params.user_id;
-    getUser(req.params.user_id).then(result => {
-        if (result.role == 'basic') // upgrade access
-            getUserPayments(userId).then(userPayments => {
+    getUser(userId).then(user => {
+        if (user.role == 'basic') // upgrade access
+            getUserPayments(user.id).then(userPayments => {
                 if (_.isEmpty(userPayments) == true) {
                     return
                 }
                 const lastPayment = userPayments.slice(-1)[0]
-                lastPayment && getPriceById(lastPayment.priceId).then(
-                    payment => {
-                        const paymentType = payment.type
-                        const paymentDate = lastPayment.payment_date
-                        const date = new Date().getTime()
-                        const diffDays = (date, otherDate) => Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
-                        const expiration = diffDays(date, paymentDate.getTime());
-                        if (paymentType == "monthly") {
-                            ((expiration < 31) == true) && updateUserRole(userId, payment.access).then(result => {
-                                return
-                            })
-                        } else if (paymentType == "yearly") {
-                            ((expiration < 366) == true) && updateUserRole(userId, payment.access).then(result => {
-                                return
-                            })
-                        }
-                    }
-                )
-            })
-        else { // downgrade access
+                lastPayment && getPriceById(lastPayment.priceId).then(payment => {
+                    const lastPaymentDate = lastPayment.payment_date.getTime();
+                    const today = new Date().getTime();
+                    const diffDays = (date, otherDate) => Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+                    const expiration = diffDays(today, lastPaymentDate);
+                    const monthExpired = (expiration > 31);
+                    const yearExpired = (expiration > 366);
+                    if (payment.type == "monthly") {
+                        (monthExpired == false) && updateUserRole(user.id, payment.access).then(result => {
+                            return
+                        });
+                    } else if (payment.type == "yearly") {
+                        (yearExpired == false) && updateUserRole(user.id, payment.access).then(result => {
+                            return
+                        });
+                    };
+                });
+            });
+        else // downgrade access
             getUserPayments(userId).then(userPayments => {
                 if (_.isEmpty(userPayments) == true) {
                     return
                 }
-                const lastPayment = userPayments.slice(-1)[0]
-                lastPayment && getPriceById(lastPayment.priceId).then(
-                    payment => {
-                        const paymentType = payment.type
-                        const paymentDate = lastPayment.payment_date
-                        const date = new Date().getTime()
-                        const diffDays = (date, otherDate) => Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
-                        const expiration = diffDays(date, paymentDate.getTime());
-                        if (paymentType == "monthly") {
-                            ((expiration > 31) == true) && updateUserRole(userId, 'basic').then(result => {
-                                return
-                            })
-                        } else if (paymentType == "yearly") {
-                            ((expiration > 366) == true) && updateUserRole(userId, 'basic').then(result => {
-                                return
-                            })
-                        }
+                const lastPayment = userPayments.slice(-1)[0];
+                lastPayment && getPriceById(lastPayment.priceId).then(payment => {
+                    const lastPaymentDate = lastPayment.payment_date.getTime();
+                    const today = new Date().getTime();
+                    const diffDays = (date, otherDate) => Math.ceil(Math.abs(date - otherDate) / (1000 * 60 * 60 * 24));
+                    const expiration = diffDays(today, lastPaymentDate);
+                    const monthExpired = (expiration > 31);
+                    const yearExpired = (expiration > 366);
+                    if (payment.type == "monthly") {
+                        (monthExpired == true) && updateUserRole(user.id, 'basic').then(result => {
+                            return
+                        })
+                    } else if (payment.type == "yearly") {
+                        (yearExpired == true) && updateUserRole(user.id, 'basic').then(result => {
+                            return
+                        })
                     }
-                )
-            })
-        }
-    })
+                });
+            });
+    });
     next()
 }
 
